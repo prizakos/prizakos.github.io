6cbbf0fbbdefef93d5fea23f07a9b776df40cb66
prizakos <peter.rizakos@gmail.com>
2026-01-21 16:04:45 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/prizakos.github.io/' // Replace with your GitHub repo name
+  base: '/' // Replace with your GitHub repo name
 }));
